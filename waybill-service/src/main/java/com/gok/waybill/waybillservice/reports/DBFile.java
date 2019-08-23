2d7678fd0f1223c4bb63a1f1b2733a50db2d29ac
Vitali Vikhliayeu <vvator@gmail.com>
2019-08-23 14:59:43 +0300

    feature: add support upload and download file in/from db
    @see
    https://www.callicoder.com/spring-boot-file-upload-download-jpa-hibernate-mysql-database-example/
    
    ## source on github
    https://github.com/callicoder/spring-boot-file-upload-download-rest-api-example/tree/mysql-database-storage

---
+++ waybill-service/src/main/java/com/gok/waybill/waybillservice/reports/DBFile.java
@@ -0,0 +1,39 @@
+package com.gok.waybill.waybillservice.reports;
+
+
+import com.gok.waybill.waybillservice.data.model.Model;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "files")
+public class DBFile extends Model {
+
+    @Column(name = "fileName")
+    private String fileName;
+    @Column(name = "fileType")
+    private String fileType;
+
+    @Lob
+    private byte[] data;
+
+    public DBFile() {
+
+    }
+
+    public DBFile(String fileName, String fileType, byte[] data) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+    }
+
+}
