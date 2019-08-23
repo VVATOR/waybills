cca4b2b4aa222346a2b8aff532210917df5e18d7
Vitali Vikhliayeu <vvator@gmail.com>
2019-08-23 17:48:43 +0300

    feature: add GraphQl query

---
+++ waybill-service/src/main/java/com/gok/waybill/waybillservice/reports/DbFile.java
@@ -0,0 +1,38 @@
+package com.gok.waybill.waybillservice.reports;
+
+import com.gok.waybill.waybillservice.data.model.Model;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "files")
+public class DbFile extends Model {
+
+    @Column(name = "fileName")
+    private String fileName;
+    @Column(name = "fileType")
+    private String fileType;
+
+    @Lob
+    private byte[] data;
+
+    public DbFile() {
+
+    }
+
+    public DbFile(String fileName, String fileType, byte[] data) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+    }
+
+}
